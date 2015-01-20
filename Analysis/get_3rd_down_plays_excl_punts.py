262be6feb627ce5f4244cfbb34fa87d923802017
Steven Wu <steven.wu.work@gmail.com>
2015-01-20 15:47:19 -0500

    File for creating a custom dataset, and the resulting .csvs

---
+++ Analysis/get_3rd_down_plays_excl_punts.py
@@ -0,0 +1,33 @@
+"""
+Simple script that will read in your MongoDB game data,
+find all 3rd down plays that didn't end in a punt,
+and write it to a .csv file for further analysis
+
+Usage:
+    get_3rd_down_plays_excl_punts.py (--season=<sn>)
+"""
+
+import csv
+from pymongo import MongoClient
+from docopt import docopt
+
+DOWN_INDEX = 1
+TYPE_INDEX = 2
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    season = args['--season']
+    client = MongoClient()
+    season_db = client['CFL_'+season]
+    games_collection = season_db['games']
+
+    filename = '{0}_3rd_down_plays_excl_punts.csv'.format(season)
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        for game in games_collection.find():
+            list_of_game_rows = game['list_of_game_rows']
+            for row in list_of_game_rows:
+                down = row[DOWN_INDEX].strip()
+                play_type = row[TYPE_INDEX].strip()
+                if down == '3' and play_type != 'Punt':
+                    writer.writerow(row)
